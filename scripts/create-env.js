7aa45456d88f3ce8a712d23aff722beb7a0d5ff7
Sebastian-Pedroza <sebaspedroza.developer@gmial.com>
2022-08-05 20:16:01 -0500

    :bug: V1.0.7
    Fix a bug in create-env.js

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 
 fs.writeFileSync(
   './.env',
-  `API_KEY=${process.env.API_KEY}\nAPI=${process.env.API}\nAPI_RANDOM=${process.env.API_RANDOM}\nAPI_RANDOM=${process.env.API_RANDOM}\nAPI_FAVORITES=${process.env.API_FAVORITES}\nAPI_UPLOAD=${process.env.API_UPLOAD}\n`
+  `API_URL=${process.env.API_URL}\nAPI_KEY=${process.env.API_KEY}\nAPI_TRENDING=${process.env.API_TRENDING}\nAPI_CATEGORIES=${process.env.API_CATEGORIES}\nAPI_DISCOVER=${process.env.API_DISCOVER}\nAPI_SEARCH=${process.env.API_SEARCH}\nAPI_MOVIE_DETAIL=${process.env.API_MOVIE_DETAIL}\nAPI_MOVIE_RECOMENDATIONS=${process.env.API_MOVIE_RECOMENDATIONS}\nAPI_MOVIE_VIDEO=${process.env.API_MOVIE_VIDEO}`
 );
